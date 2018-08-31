5dd796739122e907e876eb14b2644436273b774d
KENNETH LOW <kennethml.kml@gmail.com>
2018-08-31 23:51:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!"  
+  return "Happy holidays!";
 }
 
+function happyHlidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
 
+function happyHlidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown()
